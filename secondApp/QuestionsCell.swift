55f25e1f09260f43d17253aec50cf0af9050653b
Taiki__ck <tensainanoda@gmail.com>
2018-03-01 13:15:28 +0900

    カスタムセルの追加！

---
+++ secondApp/QuestionsCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionsCell.swift
+//  secondApp
+//
+//  Created by 加藤　大起 on 2018/03/01.
+//  Copyright © 2018年 Taiki Kato. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
